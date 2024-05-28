0580bc6f68d9486b19b4aa966ebf153734f5e94a
Benjamin Nhan <benjaminnhan12@gmail.com>
2024-05-28 14:16:54 -0700

    changes?

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,9 +52,7 @@ function initializeServiceWorker() {
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
-      navigator.serviceWorker.register('./sw.js', {
-        scope: "/",
-      })
+      navigator.serviceWorker.register('./sw.js')
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
   // B5. TODO - In the event that the service worker registration fails, console
